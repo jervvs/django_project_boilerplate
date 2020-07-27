08e1a4a47244be1f89b87f1dbbc1a2c3680f641f
Jervis <jerv0003@e.ntu.edu.sg>
2020-07-27 10:55:16 +0800

    follow-thru

---
+++ core/urls.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+from .views import item_list
+
+app_name = 'core'
+
+urlpatterns = [
+    path('', item_list, name='item-list'),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
